9b0b4d951f34420455189b365345f6f694277abb
xilingyuli <786979248@qq.com>
2017-04-05 11:08:48 +0800

    修改

---
+++ app/src/main/java/com/xilingyuli/wordstext/FileReader.java
@@ -0,0 +1,69 @@
+package com.xilingyuli.wordstext;
+
+import android.content.Context;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/**
+ * Created by xilingyuli on 2015/11/7.
+ */
+public class FileReader {
+
+    private InputStream inStream = null;
+    private InputStreamReader inputReader = null;
+    private BufferedReader bufferReader = null;
+
+    FileReader(String path, Context context)
+    {
+        try {
+            if(path!=null) {
+                File file = new File(path);
+                if (file.isDirectory())
+                    return;
+                inStream = new FileInputStream(file);
+            }
+            else
+                inStream = context.getResources().openRawResource(R.raw.words);
+            inputReader = new InputStreamReader(inStream);
+            bufferReader = new BufferedReader(inputReader);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String[] read()
+    {
+        if(bufferReader==null)
+            return null;
+        ArrayList<String> list = new ArrayList<>();
+        String line;
+        try {
+            while (( line = bufferReader.readLine()) != null) {
+                list.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return list.toArray(new String[]{""});
+    }
+
+    public void closeFile()
+    {
+        try {
+            if(bufferReader!=null)
+                bufferReader.close();
+            if(inputReader!=null)
+                inputReader.close();
+            if(inStream!=null)
+                inStream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ app/src/main/java/com/xilingyuli/wordstext/WordSwitcher.java
@@ -0,0 +1,43 @@
+package com.xilingyuli.wordstext;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.Random;
+
+/**
+ * Created by xilingyuli on 2015/11/7.
+ */
+public class WordSwitcher {
+
+    private SharedPreferences settingsPreferences;  //获取设置项
+    private String[] words;  //单词列表
+    private int index;  //上一单词位置
+
+    WordSwitcher(Context context)
+    {
+        settingsPreferences = context.getSharedPreferences("WordSettings", Activity.MODE_PRIVATE);
+        FileReader f = new FileReader(settingsPreferences.getString("path",null),context);
+        words = f.read();
+        f.closeFile();
+        index = settingsPreferences.getInt("index",0);
+    }
+    public String nextWord()
+    {
+        if(words==null||words.length==0)
+            return "";
+        if(settingsPreferences.getBoolean("isRandom",false))
+        {
+            Random r = new Random();
+            index = r.nextInt(words.length);
+            return words[index];
+        }
+        else
+            return words[(++index)%words.length];
+    }
+    public void save()
+    {
+        settingsPreferences.edit().putInt("index",index).apply();
+    }
+}
